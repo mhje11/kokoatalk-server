024cd2cf42119ab8a3c1fc01ceec6961253095dc
mhje11 <mhje1122@gmail.com>
2024-12-27 18:56:58 +0900

    fix : validation error

--- src/main/java/org/kokoatalkserver/domain/member/dto/SignUpRequestDto.java
+++ src/main/java/org/kokoatalkserver/domain/member/dto/SignUpRequestDto.java
@@ -13,13 +13,13 @@ public class SignUpRequestDto {
     @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "ID는 영어와 숫자만 입력 가능합니다.")
     private String accountId;
 
-    @NotBlank(message = "닉네임을 입력해주세요.")
-    @Size(min = 1, max = 20, message = "닉네임은 1자에서 20자 사이여야 합니다.")
-    private String password;
-
     @NotBlank(message = "비밀번호를 입력해주세요.")
     @Size(min = 8, max = 20, message = "비밀번호는 8자에서 20자 사이여야 합니다.")
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]+$",
             message = "비밀번호는 영어, 숫자, 특수문자를 포함해야 합니다.")
+    private String password;
+
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    @Size(min = 1, max = 20, message = "닉네임은 1자에서 20자 사이여야 합니다.")
     private String nickname;
 }
