68fe8e453a4fc2bf612d24bcbde18ded78548d5c
mhje11 <mhje1122@gmail.com>
2025-01-07 17:47:39 +0900

    fix : chatController

--- src/main/java/org/kokoatalkserver/domain/chatMessage/controller/ChatController.java
+++ src/main/java/org/kokoatalkserver/domain/chatMessage/controller/ChatController.java
@@ -9,8 +9,9 @@ import org.kokoatalkserver.global.util.jwt.service.RefreshTokenService;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class ChatController {
     private final ChatService chatService;
