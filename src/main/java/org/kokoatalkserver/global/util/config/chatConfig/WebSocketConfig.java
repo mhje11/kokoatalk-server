b1e6d6c6b43b9062769e07b35f2d78a470ed31c3
mhje11 <mhje1122@gmail.com>
2025-01-07 17:34:59 +0900

    fix : fix websocket config

--- src/main/java/org/kokoatalkserver/global/util/config/chatConfig/WebSocketConfig.java
+++ src/main/java/org/kokoatalkserver/global/util/config/chatConfig/WebSocketConfig.java
@@ -24,6 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
+                .addInterceptors(new JwtHandshakeInterceptor(jwtTokenizer))
                 .setAllowedOrigins("https://www.kokoatalk.shop", "https://api.kokoatalk.shop")
                 .withSockJS();
     }
