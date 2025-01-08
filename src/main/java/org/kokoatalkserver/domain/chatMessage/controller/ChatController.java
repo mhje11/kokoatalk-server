0088ff16032647724c44a6b4b3386d219c245f48
mhje11 <mhje1122@gmail.com>
2025-01-08 19:35:43 +0900

    fix : fix import

--- src/main/java/org/kokoatalkserver/domain/chatMessage/controller/ChatController.java
+++ src/main/java/org/kokoatalkserver/domain/chatMessage/controller/ChatController.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
 public class ChatController {
