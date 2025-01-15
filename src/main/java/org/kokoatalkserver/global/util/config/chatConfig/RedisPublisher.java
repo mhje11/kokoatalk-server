e4804e6f8688dbe81c7be11fa12ea3d368f4c1aa
mhje11 <mhje1122@gmail.com>
2025-01-15 20:19:17 +0900

    fix : fix redisPublisher

--- src/main/java/org/kokoatalkserver/global/util/config/chatConfig/RedisPublisher.java
+++ src/main/java/org/kokoatalkserver/global/util/config/chatConfig/RedisPublisher.java
@@ -3,6 +3,7 @@ package org.kokoatalkserver.global.util.config.chatConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.kokoatalkserver.domain.chatMessage.entity.ChatMessageRedis;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,8 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class RedisPublisher {
-    private final RedisTemplate<String, Object> redisTemplate;
+    @Qualifier("chatRoomRedisTemplate")
+    private final RedisTemplate<String, ChatMessageRedis> redisTemplate;
 
     public void publish(ChannelTopic channelTopic, ChatMessageRedis message) {
         log.info("published topic = {}", channelTopic.getTopic());
