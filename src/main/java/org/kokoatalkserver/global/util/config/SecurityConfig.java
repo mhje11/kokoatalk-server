36fab597a3da51139e280b9543af88653223c32e
mhje11 <mhje1122@gmail.com>
2025-03-09 20:31:02 +0900

    feat: cors config

--- src/main/java/org/kokoatalkserver/global/util/config/SecurityConfig.java
+++ src/main/java/org/kokoatalkserver/global/util/config/SecurityConfig.java
@@ -131,7 +131,6 @@ public class SecurityConfig {
         config.addAllowedOrigin("https://api.kokoatalk.shop");
         config.addAllowedOrigin("http://localhost:8080");
         config.addAllowedOrigin("https://kokoatalk-server-c794b03f124a.herokuapp.com");
-//        config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true); // 쿠키 허용을 위해 필요
