b1d7b7e42caad1820b485e75e32a309da14ad036
mhje11 <mhje1122@gmail.com>
2025-03-12 18:00:13 +0900

    fix : fix cors config

--- src/main/java/org/kokoatalkserver/global/util/config/SecurityConfig.java
+++ src/main/java/org/kokoatalkserver/global/util/config/SecurityConfig.java
@@ -129,10 +129,11 @@ public class SecurityConfig {
     public CorsConfigurationSource configurationSource() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("https://www.kokoatalk.shop");
-        config.addAllowedOrigin("https://api.kokoatalk.shop");
-        config.addAllowedOrigin("http://localhost:8080");
-        config.addAllowedOrigin("https://kokoatalk-server-c794b03f124a.herokuapp.com");
+        config.setAllowedOriginPatterns(List.of("*"));
+//        config.addAllowedOrigin("https://www.kokoatalk.shop");
+//        config.addAllowedOrigin("https://api.kokoatalk.shop");
+//        config.addAllowedOrigin("http://localhost:8080");
+//        config.addAllowedOrigin("https://kokoatalk-server-c794b03f124a.herokuapp.com");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true); // 쿠키 허용을 위해 필요
