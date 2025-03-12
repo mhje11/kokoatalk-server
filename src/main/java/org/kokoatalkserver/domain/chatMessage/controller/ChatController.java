0c9cfcf3272f314906372cc72e30c6382473c56e
mhje11 <mhje1122@gmail.com>
2025-03-12 20:55:54 +0900

    feat : add swagger docs

--- src/main/java/org/kokoatalkserver/domain/chatMessage/controller/ChatController.java
+++ src/main/java/org/kokoatalkserver/domain/chatMessage/controller/ChatController.java
@@ -1,5 +1,12 @@
 package org.kokoatalkserver.domain.chatMessage.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.kokoatalkserver.domain.chatMessage.dto.ChatMessageGetDto;
 import org.kokoatalkserver.domain.chatMessage.dto.ChatMessageScrollDto;
@@ -18,21 +25,30 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Chat", description = "채팅 관련 API")
 public class ChatController {
     private final ChatService chatService;
     private final ChatMessageService chatMessageService;
 
+    @Operation(summary = "채팅 메시지 전송 (WebSocket)",
+    description = "WebSocket을 사용하여 특정 채팅방에 메시지를 전송" + "\n" +
+            "해당 엔드포인트는 WebSocket 연결이 필요하며, STOMP 프로토콜을 사용해야 함")
     @MessageMapping("/chat/send/{roomId}")
     public void sendMessage(Principal principal, ChatMessageSendDto chatMessageSendDto, @DestinationVariable String roomId) {
         Long kokoaId = Long.valueOf(principal.getName());
         chatService.sendMessage(kokoaId, roomId, chatMessageSendDto.getMessage(), chatMessageSendDto.getImageUrls());
     }
+
+    @Operation(summary = "채팅 이미지 업로드", description = "채팅에서 사용할 이미지를 업로드")
+    @ApiResponse(responseCode = "200", description = "채팅방에서 사용할 이미지를 업로드 하는 기능", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = String.class))))
     @PostMapping("/api/chat/image/upload")
     public ResponseEntity<List<String>> uploadChatImages(@RequestPart List<MultipartFile> multipartFile) {
         List<String> imageUrls = chatService.uploadFiles(multipartFile);
         return ResponseEntity.ok(imageUrls);
     }
 
+    @Operation(summary = "채팅 메시지 조회", description = "특정 채팅방의 메시지를 가져오며 무한 스크롤 방식 이용을 위한 API")
+    @ApiResponse(responseCode = "200", description = "채팅방 메시지 조회 성공", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ChatMessageScrollDto.class))))
     @GetMapping("/api/chatRoom/{roomId}/messages")
     public ResponseEntity<List<ChatMessageScrollDto>> getChatMessages(@RequestBody ChatMessageGetDto chatMessageGetDto, @PathVariable String roomId) {
         LocalDateTime lastCreatedAt = chatMessageGetDto.getLastCreatedAt() != null ? chatMessageGetDto.getLastCreatedAt() : LocalDateTime.now();
@@ -40,6 +56,8 @@ public class ChatController {
         return ResponseEntity.ok(messages);
     }
 
+    @Operation(summary = "채팅 메시지 백업", description = "채팅 메시지를 Redis 에서 MySql 로 백업, 실제로는 스케줄러를 사용함")
+    @ApiResponse(responseCode = "200", description = "백업 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
     @PostMapping("/api/chatMessage/backup")
     public ResponseEntity<String> triggerBackup() {
         chatMessageService.archiveChatMessages();
--- src/main/java/org/kokoatalkserver/domain/chatRoom/controller/ChatRoomRestController.java
+++ src/main/java/org/kokoatalkserver/domain/chatRoom/controller/ChatRoomRestController.java
@@ -1,5 +1,12 @@
 package org.kokoatalkserver.domain.chatRoom.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.kokoatalkserver.domain.chatRoom.dto.AddMemberDto;
 import org.kokoatalkserver.domain.chatRoom.dto.ChatRoomCreateDto;
@@ -16,27 +23,36 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/chatRoom")
+@Tag(name = "ChatRoom", description = "채팅방 관련 API")
 public class ChatRoomRestController {
     private final ChatRoomService chatRoomService;
 
+    @Operation(summary = "채팅방 생성", description = "초대할 회원을 선택해 채팅방 생성")
+    @ApiResponse(responseCode = "200", description = "채팅방 생성 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
     @PostMapping("/create")
     public ResponseEntity<String> createChatRoom(@RequestBody ChatRoomCreateDto chatRoomCreateDto) {
         Long roomId = chatRoomService.createChatRoom(chatRoomCreateDto.getRoomName(), chatRoomCreateDto.getFriendCodeList());
         return ResponseEntity.ok("채팅방 생성 완료 roomId: " + roomId);
     }
 
+    @Operation(summary = "채팅방 목록 조회", description = "유저의 채팅방 목록을 조회")
+    @ApiResponse(responseCode = "200", description = "채팅방 목록 조회 성공", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ChatRoomInfoDto.class))))
     @GetMapping("/list")
     public ResponseEntity<List<ChatRoomInfoDto>> getChatRoomList(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
         List<ChatRoomInfoDto> roomList = chatRoomService.getRoomList(customUserDetails.getUserId());
         return ResponseEntity.ok(roomList);
     }
 
+    @Operation(summary = "채팅방 떠나기", description = "채팅방 나가기, 해당 채팅방의 인원이 0명이 되면 채팅방 삭제")
+    @ApiResponse(responseCode = "200", description = "채팅방 나가기", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
     @PostMapping("/leave")
     public ResponseEntity<String> leaveRoom(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody ChatRoomLeaveDto chatRoomLeaveDto) {
         chatRoomService.leaveRoom(customUserDetails.getUserId(), chatRoomLeaveDto.getRoomId());
         return ResponseEntity.ok("방 떠나기 성공");
     }
 
+    @Operation(summary = "채팅방에 멤버 초대", description = "채팅방 멤버초대, 1 : 1 채팅방일경우 그룹 채팅방으로 새로운 채팅방 생성")
+    @ApiResponse(responseCode = "200", description = "채팅방 멤버 초대 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
     @PostMapping("/addMember")
     public ResponseEntity<String> addMember(@RequestBody AddMemberDto addMemberDto) {
         chatRoomService.checkRoomType(addMemberDto.getRoomId(), addMemberDto.getNewFriendCode());
